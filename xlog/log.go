869a8b556fae93fb40b9a94f33a613f90c1ef21c
myxy99 <2399158611@qq.com>
2021-04-16 15:28:36 +0800

    normalizeMessage

--- xlog/log.go
+++ xlog/log.go
@@ -143,7 +143,7 @@ func (logger *Logger) Debugf(template string, args ...interface{}) {
 }
 
 func normalizeMessage(msg string) string {
-	return fmt.Sprintf("%-16s", msg)
+	return fmt.Sprintf("%-32s", msg)
 }
 
 // Info ...
